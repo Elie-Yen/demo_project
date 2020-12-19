2bf99a85e8a77f0a48b607c9eda5a1f285e87104
Elie-Yen <60340631+Elie-Yen@users.noreply.github.com>
2020-12-19 15:38:18 +0800

    Update Conway_game.py

--- Game/Conway_game.py
+++ Game/Conway_game.py
@@ -5,75 +5,61 @@ Conway's Game of life
 '''
 import numpy
 import math
-import timeit
 def get_generation(cells, generations):
-    if len(cells) < 1:
-        return []
 
     #_ the direction of adjacent cells
-    adj = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1),
-           (1, -1), (1, 0), (1, 1))
-    #_ store how many live neighbors, > 0: current living cells
-    adj_live = dict(((r, c), 2 * cells[r][c] - 1)
-                for c in range(len(cells[0])) for r in range(len(cells)))
-    print(cells)
-    for _ in range(generations):
+    adj = ((-2, -2), (-2, -1), (-2, 0), (-1, -2), (-1, 0),
+           (0, -2), (0, -1), (0, 0))
+
+    def status(cells, cur):
+
+        print("\ngeneration{0}\n".format(cur), cells)
+        if not generations or len(cells) < 1 or cur == generations:
+            return cells
+        
+        #_ expand 1 cells in each border
+        #_ 1 for live cells, -1 for dead cells
         h, w = len(cells), len(cells[0])
-        
-        #_ calculate how many adj live cells
-        for row in range(h):
-            for col in range(w):
-                if not cells[row][col]:
-                    continue
-                for r, c in adj:
-                    if (row + r, col + c) in adj_live:
-                        adj_live[(row + r, col + c)] *= 2
-                        #_ live/ dead cells that has 3+ live neighbors will die
-                        if adj_live[(row + r, col + c)] in (16, -16):
-                            del adj_live[(row + r, col + c)]
-                        
-                    #_ live cell is on boarder, consider expand bc
-                    #_ the original dead cell might become alive
-                    elif row + r in (-1, h) or col + c in (-1, w):
-                        adj_live[(row + r, col + c)] = -2
-        
-        #_ check the status of next generation
-        #_ next generation's range of width, height after trim empty row/ col
+        next_cells = numpy.full((h + 2, w + 2), -1, dtype = numpy.int8)
+        next_cells[1: -1, 1: -1] = cells[:]
+
+        #_ new height, width of next generation
+        nh, nw = -math.inf, -math.inf
         min_h, min_w = math.inf, math.inf
-        h, w = -math.inf, -math.inf
-        new_live = set()
-        for row, col in adj_live:
-            #_ live cell with 2/3 live neighbors/ dead cell with 3 neighbors
-            if adj_live[row, col] in (4, 8, -8):
-                h, min_h = max(h, row), min(min_h, row)
-                w, min_w = max(w, col), min(min_w, col)
-                new_live.add((row, col))
+        
+        for row in range(len(next_cells)):
+            for col in range(len(next_cells[0])):
+                 #_ calculate how many adj live cells
+                 #_ next_cells[i + 1][j + 1] = cells[i][j]
+                for r, c in adj:
+                    if (-1 < row + r < h and -1 < col + c < w and
+                        cells[row + r, col + c]):
+                        next_cells[row, col] *= 2
+                        #_ cells that have 3+ live neighbors will die
+                        if next_cells[row, col] in (16, -16):
+                            next_cells[row, col] = 0 
+                            break
 
-        #_ no live cells in next generation
-        if not new_live:
-            print([])
-            return []
+                #_ check next status of cell by its value
+                #_ update range of width, height after trim empty row/ col
+                if next_cells[row, col] in (4, 8, -8):
+                    nh, min_h = max(nh, row), min(min_h, row)
+                    nw, min_w = max(nw, col), min(min_w, col)
+                    next_cells[row, col] = 1
+                else:
+                    next_cells[row, col] = 0 
 
-        #_ update the adj_live of next generation
-        cells = []
-        adj_live = dict()
-        for r in range(h - min_h + 1):
-            tmprow = []
-            for c in range(w - min_w + 1):
-                Val = int((r + min_h, c + min_w) in new_live)
-                adj_live[((r, c))] = Val * 2 - 1
-                tmprow.append(Val)
-            cells.append(tmprow)
+        #_ if no live cells, cells = []
+        #_ else trim the empty rows/ cols of next generation
+        cells = ([] if min_h == min_w == -nh == -nw == math.inf
+                else next_cells[min_h: nh + 1, min_w: nw + 1])
+        status(cells, cur + 1)
 
-        print(cells)
-    
-    return cells
-                    
+    return status(cells, 0) 
 
 
 
 
-#_ for test the program
-cells = numpy.random.randint(2, size=(20, 6))
-get_generation(cells, 20)
-
+#_ test
+cells = numpy.random.randint(2, size=(3, 5))
+get_generation(cells, 5)
