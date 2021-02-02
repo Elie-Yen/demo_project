62e55937e2dfdd087c6d850fac8806e7cbe80514
Elie Yen <60340631+Elie-Yen@users.noreply.github.com>
2021-02-02 13:47:34 +0800

    Add files via upload

---
+++ Math/password_generator.cs
@@ -0,0 +1,168 @@
+class Password {
+    private int _length;
+    private bool _uppercase;
+    private bool _lowercase;
+    private bool _digit;
+    private bool _symbol;
+    private bool _space;
+    private string _other;
+    private int _permutation_time;
+    
+    public int length { get => _length; set{_length = value;} }
+    public bool uppercase { get => _uppercase; set{_uppercase = value;} }
+    public bool lowercase { get => _lowercase; set{_lowercase = value;} }
+    public bool digit { get => _digit; set{_digit = value;} }
+    public bool symbol { get => _symbol; set{_symbol = value;} }
+    public bool space { get => _space; set{_space = value;} }
+    public string other { get => _other; set{_other = value;} }
+    public int permutation_time { get => _permutation_time; set{_permutation_time = value;} }
+
+    public Password(
+        int len = 16,  bool upper = true,  bool lower = true,  bool num = true,  bool special = false,  bool spce = false,  string oth = "" ,  int permutation = 0){
+        Console.WriteLine(@"Welcome! Generate customized password by setting attributes below:
+        Set a int for the length of your password : [length]. (5 < value < 100)
+        Set true if used otherwise false: [uppercase], [lowercase], [digit], [symbol], [space].
+        Set a string if the chars you want to used are not in the list above: [other].
+        Set a int if you want to permutate the result: [permutation_time]. (0 < value < (length !) )
+
+        Default attrubutes: length = 16, use only uppercases, lowercases and digits without permutation.
+        If you set up already, use [ Generate() ] to generate.");
+        
+        length = len;
+        uppercase = upper;
+        lowercase = lower;
+        digit = num;
+        symbol = special;
+        space = spce;
+        other = oth;
+        permutation_time = permutation;
+
+        string sample = Generate();
+        Console.WriteLine($"This is sample: {sample}");
+    }
+
+    public String Generate(){
+        Dictionary<int, string> charset = new Dictionary<int, string>{
+                [0] = "abcdefghijklmnopqrstuvwxyz",
+                [1] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 
+                [2] = "0123456789",           
+                [3] = "~!@#$%^&*_-+=`|(){}[]:;\"'<>,.?/\\",
+                [4] = "          ",
+                [5] = other
+        };    
+        String res = "";
+        bool[] option = new bool[]{lowercase, uppercase, digit, symbol, space, other.Length > 0};
+        List<string> used = new List<string>();
+        for (int i = 0; i < 6; i++){
+            if (option[i]){
+                used.Add(charset[i]);
+            }
+        }
+        // valid inputs
+        if (length < 6){
+            Console.WriteLine("Error, Set higher length for your security.");
+        }
+        else if (used.Count < 2){
+            Console.WriteLine(
+                "Error, must choose at least 2 types of character to generate the password.");
+        }
+        else {
+            StringBuilder password = new StringBuilder("");
+            //Random rnd = new Random();
+
+            // the position to be filled
+            List<int> pos = new List<int>();
+            for (int i = 0; i < length; i++){
+                pos.Add(i);
+                password.Append("_");
+            }
+
+            int unfilled = length;
+            int avgtime = (length / used.Count > 2)? length / used.Count : 1;
+            
+            // ensure at least one required char is used
+            foreach (string chars in used){
+                for (int times = 0; times < avgtime ; times++){
+                    int x = System.Security.Cryptography.RandomNumberGenerator.GetInt32(pos.Count);
+                    int j = pos[x];
+                    pos.Remove(j);
+                    int m = System.Security.Cryptography.RandomNumberGenerator.GetInt32(chars.Length);
+                    password[j] = chars[m];
+                    }
+                unfilled -= avgtime;
+            }
+
+            // randomly pick from used charset
+            for (int i = 0; i < unfilled; i++){
+                int x = System.Security.Cryptography.RandomNumberGenerator.GetInt32(pos.Count);
+                int j = pos[x];
+                pos.Remove(j);
+                int k = System.Security.Cryptography.RandomNumberGenerator.GetInt32(used.Count);
+                int m = System.Security.Cryptography.RandomNumberGenerator.GetInt32(used[k].Length);
+                password[j] = used[k][m];
+            }
+            res = password.ToString();
+        }
+        return res;
+    }
+
+    public String Permutation(string s, int times){
+        // return kth permutation by lexicographical order 
+        String res = "";
+        //Dictionary<char, int> cnt = new Dictionary<char, int>();
+        List<char> order = new List<char>();
+        List<int> facto = new List<int>();
+        foreach (char x in s){
+            order.Add(x);
+            facto.Add(0);
+        }
+        // turn times into factoradic representation
+        int n = 1;
+        for (int i = s.Length - 1; i > -1; i--){
+            facto[i] = times % n;
+            times /= n;
+            n ++;
+            if (times == 0){
+                break;
+            }
+        }
+        foreach (int i in facto){
+            if (i >= order.Count){
+                // times exceed maximum ways of permutation
+                s = _Reverse(s);
+                return s;
+            }
+            res += order[i];
+            order.Remove(order[i]);
+        } 
+        return res;
+        
+    }
+
+    private string _Reverse(string s){
+        StringBuilder res = new StringBuilder(s);
+        for (int i = 0; i < s.Length / 2; i ++){
+            char tmp = res[i];
+            res[i] = res[s.Length - i - 1];
+            res[s.Length - i - 1] = tmp;
+        }
+        return res.ToString();
+    }
+}
+
+class Example {
+    static void Main() {
+        // for printing unicodes
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        var ex = new Password();
+        ex.length = 25;
+        ex.symbol = true;
+        ex.other = "➌✿";
+
+        var res = ex.Generate();
+        Console.WriteLine(res); // Ub➌6yH1a@I0f9'4➌K.;➌➌O✿*h
+
+        res = ex.Permutation(res, 3628799); 
+        Console.WriteLine(res); // Ub➌6yH1a@I0f9'4h*✿O➌➌➌;.K
+    }
+}
---
+++ Math/regular_number_generator.cs
@@ -0,0 +1,116 @@
+/*------
+C#
+Elie yen
+------*/
+
+public class HammingNumber{
+    // field
+    public static int MaxK { get => 1691; }
+    public static int MinK { get => 1; }
+    public static int MaxHammingNumber{ get => 2125764000; }
+    public static int MinHammingNumber{ get => 1; }
+    
+    // internal assistance programs
+    private static int MinofThree(int n1, int n2, int n3){
+        if (n1 <= n2 && n1 < n3){
+            return n1;
+        }
+        if (n2 <= n1 && n2 < n3){
+            return n2;
+        }
+        return n3;
+    }
+    private static int[] DecomposeHelper(int n){
+        // this is for validation, decomposition
+        // each int in res stands for: power of 2/3/5 and n mods
+        // if n is not a Hamming number, the last int is not 1
+        int[] res = new int[]{0, 0, 0, 0};
+        while (n % 5 == 0){
+            res[2] ++;
+            n /= 5;
+        }
+        while (n % 3 == 0){
+            res[1] ++;
+            n /= 3;
+        }
+        while (n % 2 == 0){
+            res[0] ++;
+            n /= 2;
+        }
+        res[3] = n;
+        return res;
+    }
+    public static int[] GenerateKthList(int k){
+        // this is also for generate kth hamming number
+        if (k > MaxK || k < MinK){
+            throw new ArgumentOutOfRangeException("Error, k is out of range(1 ~ 1691)");
+        }
+         
+        int hm = 1;
+        List<int> history = new List<int>();
+        history.Add(hm);
+
+        int i2 = 0;
+        int i3 = 0;
+        int i5 = 0; 
+
+        for (int i = 1; i < k; i++){
+            hm = MinofThree(history[i2] * 2, history[i3] * 3, history[i5] * 5);
+            history.Add(hm);
+            if (history[i2] * 2 == hm){
+                i2 ++;
+            }
+            if (history[i3] * 3 == hm){
+                i3 ++;
+            }
+            if (history[i5] * 5 == hm){
+                i5 ++;
+            }
+        }
+        return history.ToArray();
+    }
+    public static int GenerateKthNum(int k){
+        int[] res = GenerateKthList(k);
+        return res[k - 1];
+    }
+    public static bool Valid(int n){
+        int[] powers = DecomposeHelper(n);
+        return powers[3] == 1;
+    }
+    public static int[] Decompose(int n){
+        int[] powers = DecomposeHelper(n);
+        if (powers[3] != 1){
+            Console.WriteLine($"Error, {n} is not a valid Hamming number.");
+            powers = Array.Empty<int>(); // return an empty array
+        }
+        else {
+            Console.WriteLine($"{n} = 2 ^ {powers[0]} * 3 ^ {powers[1]} * 5 ^ {powers[2]}");
+            Array.Resize(ref powers, 3);
+        }
+        return powers;
+    }
+}
+
+class Test {
+    static void Main() {
+        int ex = HammingNumber.GenerateKthNum(1500);
+        Console.WriteLine(ex); // 859963392
+        HammingNumber.Decompose(ex);
+        // return int array: {17, 8, 5}
+        // print: 859963392 = 2 ^ 17 * 3 ^ 8 * 5 ^ 0
+
+        ex = HammingNumber.GenerateKthNum(13);
+        Console.WriteLine(HammingNumber.Valid(ex * 10)); // true
+
+        int[] first_hamming_50 = HammingNumber.GenerateKthList(50);
+        foreach (int hm in first_hamming_50){
+             Console.WriteLine(hm);
+        }
+        /* -----------
+         [1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 18, 20, 24, 25, 27,
+          30, 32, 36, 40, 45, 48, 50, 54, 60, 64, 72, 75, 80, 81, 90,
+          96, 100, 108, 120, 125, 128, 135, 144, 150, 160, 162, 180,
+          192, 200, 216, 225, 240, 243]
+        -------*/
+    }
+}
